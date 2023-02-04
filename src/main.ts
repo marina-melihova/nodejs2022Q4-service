29e4b555d23f51ac1355ca45a57b5744548451b9
Marina-Melihova <marinamelihova29@gmail.com>
2023-02-04 14:15:27 +0200

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule } from '@nestjs/swagger';
+import { readFile } from 'fs/promises';
+import * as path from 'path';
+import * as yaml from 'yaml';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  const dir = process.cwd();
+  const apiPath = path.resolve(process.cwd(), './doc/api.yaml');
+  const apiFile = await readFile(apiPath, { encoding: 'utf8' });
+  const schema = yaml.parse(apiFile);
+  SwaggerModule.setup('api', app, schema);
   await app.listen(4000);
 }
 bootstrap();
