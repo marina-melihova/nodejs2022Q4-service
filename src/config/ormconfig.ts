74ca4e8664e6a1b82a96ffb94303f8e9fcce0d94
marina-melihova <melihova.mary@gmail.com>
2023-02-26 18:07:59 +0200

    refactor: ormconfig

--- src/config/ormconfig.ts
+++ src/config/ormconfig.ts
@@ -7,19 +7,16 @@ dotenv.config();
 export const configService = {
   type: 'postgres',
   host: process.env.POSTGRES_HOST,
-  port: +process.env.POSTGRES_PORT || 5432,
+  port: +process.env.POSTGRES_PORT,
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
   entities: [User, Artist, Album, Track, Favorites],
   synchronize: false,
-  migrationsRun: false,
+  migrationsRun: true,
   logging: true,
   logger: 'file',
   migrations: ['src/migrations/**/*.ts'],
-  cli: {
-    migrationsDir: 'src/migrations',
-  },
 } as DataSourceOptions;
 
 export default new DataSource(configService);
